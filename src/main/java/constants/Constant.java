13e2a4aca762a0a0437c1d4f5eea0a24291b211e
IgorSologub <sologub355@gmail.com>
2022-09-08 13:05:02 +0300

    gg24 TestNG

--- src/main/java/constants/Constant.java
+++ src/main/java/constants/Constant.java
@@ -91,7 +91,10 @@ public class Constant {
                 "Exciting chats with hot women",
                 "Casualdating",
                 "Loveeto.com",
-                "Find the one who's right for you"
+                "Find the one who's right for you",
+                "BeSexcited",
+                "My Dirty Neighbour",
+                "Online Hookups"
         );
         public static final int NUMBER_OF_DOWNLOAD_APP_REG_BUTTONS = 5;
         public static final int NUMBER_OF_VISIT_SITE_REG_BUTTONS = 24;
